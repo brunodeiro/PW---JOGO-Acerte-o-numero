179f6d1b28b3233db7bf0a442048e67d5678e9b3
brunodeiro <bruno_deiro@live.com>
2020-03-18 17:23:40 -0300

    Criação do da função 1 jogador

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,41 @@
 var inputNome = document.getElementById("inputNome")
 var tentativas = 0
 var game = true
+
+// FUNÇÃO PARA 1 JOGADOR
+function jogar1(){
+    // VALIDAÇÃO PARA CADASTRAMENTO DO NOME
+    if (inputNome.value){    
+        var menu = document.getElementById("menu")
+        menu.style.display="none"
+
+        var jogo = document.getElementById("jogo")
+        // JOGO
+        var numeroSecreto = document.createElement("input")
+        numeroSecreto.id="numeroSecreto"
+        numeroSecreto.setAttribute("type", "password")
+        numeroSecreto.value = Math.floor((Math.random() * 100) + 1);     
+        numeroSecreto.style.display="none" 
+        jogo.appendChild(numeroSecreto)
+        
+        var numeroJogador = document.createElement("input")
+        numeroJogador.id="numeroJogador"
+        numeroJogador.setAttribute("type", "text")
+        numeroJogador.setAttribute("placeholder", "Insira aqui o suposto numero")
+        jogo.appendChild(numeroJogador)
+
+        var btn = document.createElement("button")
+        btn.id="btn"
+        btn.innerText="Go"
+        btn.setAttribute("onclick", "rodar()")
+        jogo.appendChild(btn)
+
+    }
+    else{
+        alert("Digite o seu nome antes de começar!")
+    }
+}
+
 // FUNÇÃO PARA 2 JOGADORES
 function jogar2(){
     // VALIDAÇÃO PARA CADASTRAMENTO DO NOME
@@ -43,25 +78,30 @@ function rodar(){
             var ul = document.getElementById("ul")
             var li = document.createElement("li")
 
-            if (Number(numeroSecreto.value) > Number(numeroJogador.value)){
-                 li.innerHTML = inputNome.value+", " +numeroJogador.value+" é um Valor MENOR que o numero secreto"
-                 ul.appendChild(li)
-             }
-             if (Number(numeroSecreto.value) < Number(numeroJogador.value)){
-                 li.innerHTML =  inputNome.value+", " +numeroJogador.value+" é um Valor MAIOR que o numero secreto"
-                 ul.appendChild(li)
-             }
-             if (Number(numeroSecreto.value) == Number(numeroJogador.value)){
-                 li.innerHTML =  inputNome.value+", " +numeroSecreto.value+" é o numero exato!! Você acertou após: "+tentativas+" Tentativa(s)"
-                 ul.appendChild(li)
-                 game = false // ao finalizar o game não vai ser executado o bloco if
-                 var btnRank = document.createElement("button")
-                 btnRank.id="btnRank"
-                 btnRank.innerText="Ranking"
-                 btnRank.setAttribute("onclick", "rank()")
-                 ul.appendChild(btnRank)
-                
-             }  
+            if(numeroJogador.value > 99){
+                alert("Numero acima do limite (99)")
+            }
+            else{            
+                if (numeroSecreto.value > numeroJogador.value){
+                    li.innerHTML = inputNome.value+", " +numeroJogador.value+" é um Valor MENOR que o numero secreto"
+                    ul.appendChild(li)
+                }
+                if (numeroSecreto.value < numeroJogador.value){
+                    li.innerHTML =  inputNome.value+", " +numeroJogador.value+" é um Valor MAIOR que o numero secreto"
+                    ul.appendChild(li)
+                }
+                if (numeroSecreto.value == numeroJogador.value){
+                    li.innerHTML =  inputNome.value+", " +numeroSecreto.value+" é o numero exato!! Você acertou após: "+tentativas+" Tentativa(s)"
+                    ul.appendChild(li)
+                    game = false // ao finalizar o game não vai ser executado o bloco if
+                    var btnRank = document.createElement("button")
+                    btnRank.id="btnRank"
+                    btnRank.innerText="Ranking"
+                    btnRank.setAttribute("onclick", "rank()")
+                    ul.appendChild(btnRank)
+                    
+                }  
+            }
         }
     }
     else{
